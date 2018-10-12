c8717ee4ba0e174279206c18ed853b46264aef9b
Watcharit Tuntayakul <watcharit.new@gmail.com>
2018-10-12 21:13:05 +0700

    Add assets and add TestCharacter class for test animation

---
+++ core/src/com/gameprogmeth/game/TestCharacter.java
@@ -0,0 +1,63 @@
+package com.gameprogmeth.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+
+public class TestCharacter implements Screen {
+
+	Texture img; 
+	private GameProgMeth game;
+	
+	public TestCharacter(GameProgMeth game) {
+		this.game = game;
+	}
+	
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		img = new Texture("Armored Bug.png");
+	}
+
+	@Override
+	public void render(float delta) {
+		// TODO Auto-generated method stub
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		game.batch.begin();
+		game.batch.draw(img, 0, 0);
+		game.batch.end();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- desktop/src/com/gameprogmeth/game/desktop/DesktopLauncher.java
+++ desktop/src/com/gameprogmeth/game/desktop/DesktopLauncher.java
@@ -7,6 +7,10 @@ import com.gameprogmeth.game.GameProgMeth;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.foregroundFPS = 60;
+		config.width = GameProgMeth.WIDTH;
+		config.height = GameProgMeth.HEIGHT;
+		config.fullscreen = true;
 		new LwjglApplication(new GameProgMeth(), config);
 	}
 }
