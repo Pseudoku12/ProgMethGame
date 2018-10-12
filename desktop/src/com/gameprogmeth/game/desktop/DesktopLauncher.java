9b7a26b439086a59ce087de8df06ec3605b1287e
PURINUT THEDWICHIENCHAI <purinatth@gmail.com>
2018-10-12 16:33:05 +0700

    initialization

---
+++ desktop/src/com/gameprogmeth/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gameprogmeth.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gameprogmeth.game.GameProgMeth;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameProgMeth(), config);
+	}
+}
