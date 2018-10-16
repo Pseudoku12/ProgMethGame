cc866f7aeb8d711b3e1e7d3b535eea8fd7d47db8
Watcharit Tuntayakul <watcharit.new@gmail.com>
2018-10-16 22:07:43 +0700

    Can edit map with tile

--- core/src/com/gameprogmeth/game/GameProgMeth.java
+++ core/src/com/gameprogmeth/game/GameProgMeth.java
@@ -2,6 +2,7 @@ package com.gameprogmeth.game;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.gameprogmeth.game.world.MapNormal;
@@ -28,7 +29,14 @@ public class GameProgMeth extends Game {
 	}
 
 	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		//super.render();
+		if(Gdx.input.isTouched()) {
+			cam.translate(Gdx.input.getDeltaX(),Gdx.input.getDeltaY());
+			cam.update();
+		}
+		
 		
 		gameMap.render(cam);
 	}
