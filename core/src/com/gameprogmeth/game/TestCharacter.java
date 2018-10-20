24f0413b101c97cda963b5049a40f421520cf770
PURINUT THEDWICHIENCHAI <purinatth@gmail.com>
2018-10-20 22:19:14 +0700

    TestCharacter.java modified

--- core/src/com/gameprogmeth/game/TestCharacter.java
+++ core/src/com/gameprogmeth/game/TestCharacter.java
@@ -1,6 +1,7 @@
 package com.gameprogmeth.game;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
@@ -13,11 +14,12 @@ public class TestCharacter implements Screen {
 	public static final float Charater_WIDTH = 120;
 	public static final float Charater_HEIGHT = 80;
 	public static final int GHOST_WIDTH_PIXEL = 16;
-	public static final int GHOST_HEIGHT_PIXEL = 22;
+	public static final int GHOST_HEIGHT_PIXEL = 24;
 	
 	private float x;
 	private float y;
 	private float stateTime;
+	private float speed;
 	
 	private Animation<TextureRegion>[] character;
 	private int roll;
@@ -28,12 +30,16 @@ public class TestCharacter implements Screen {
 		
 		x = 100;
 		y = 100;
+		speed = 5;
 		
 		character = new Animation[4];
-		roll = 0;
+		roll = 2;
 		TextureRegion[][] rollSpriteSheet = TextureRegion.split(new Texture("Ghost.png"), GHOST_WIDTH_PIXEL, GHOST_HEIGHT_PIXEL);
 		
-		character[roll] = new Animation<TextureRegion>(GHOST_ANIMATION_SPEED, rollSpriteSheet[roll]);
+		character[0] = new Animation<TextureRegion>(GHOST_ANIMATION_SPEED, rollSpriteSheet[0]);
+		character[1] = new Animation<TextureRegion>(GHOST_ANIMATION_SPEED, rollSpriteSheet[1]);
+		character[2] = new Animation<TextureRegion>(GHOST_ANIMATION_SPEED, rollSpriteSheet[2]);
+		character[3] = new Animation<TextureRegion>(GHOST_ANIMATION_SPEED, rollSpriteSheet[3]);
 		
 	}
 	
@@ -49,7 +55,19 @@ public class TestCharacter implements Screen {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 		stateTime += delta;
-		
+		if(Gdx.input.isKeyPressed(Input.Keys.W)) {
+			y += speed;
+			roll = 2;
+		} else if(Gdx.input.isKeyPressed(Input.Keys.A)) {
+			x -= speed;
+			roll = 3;
+		} else if(Gdx.input.isKeyPressed(Input.Keys.S)) {
+			y -= speed;
+			roll = 0;
+		} else if(Gdx.input.isKeyPressed(Input.Keys.D)) {
+			x += speed;
+			roll = 1;
+		}
 		game.batch.begin();
 		game.batch.draw(character[roll].getKeyFrame(stateTime, true), x, y, Charater_WIDTH, Charater_HEIGHT);
 		game.batch.end();
