45a7594386197cae31735c64def375aa7598d6a4
Watcharit Tuntayakul <watcharit.new@gmail.com>
2018-11-06 22:21:36 +0700

    Add level 4

--- core/src/com/gameprogmeth/game/world/custommap/CustomGameMapLoader.java
+++ core/src/com/gameprogmeth/game/world/custommap/CustomGameMapLoader.java
@@ -57,7 +57,7 @@ public class CustomGameMapLoader {
 		int originX = 50;
 		int originY = 50;
 		
-		findTheWay(originX, originY, 150);
+		findTheWay(originX, originY, 100);
 		
 		
 		for (int row = 0; row < SIZE; row++) {
@@ -112,100 +112,29 @@ public class CustomGameMapLoader {
 			int x = originX;
 			x--;
 			Triple pairCo = new Triple(randomMap[x][originY],x,originY);
-//			chkMap[originX-1][originY] = 1;
 			pqMap.add(pairCo);	
 		}
 		if(originX+1 < SIZE) {
 			int x = originX;
 			x++;
 			Triple pairCo = new Triple(randomMap[x][originY],x,originY);
-//			chkMap[originX+1][originY] = 1;
 			pqMap.add(pairCo);
 		}
 		if(originY-1 >=0) {
 			int y = originY;
 			y--;
 			Triple pairCo = new Triple(randomMap[originX][y],originX,y);
-//			chkMap[originX][originY-1] = 1;
 			pqMap.add(pairCo);
 		}
 		if(originY+1 < SIZE) {
 			int y = originY;
 			y++;
 			Triple pairCo = new Triple(randomMap[originX][y],originX,y);
-//			chkMap[originX][originY+1] = 1;
 			pqMap.add(pairCo);
 			
 		}
 		Triple point = pqMap.poll();
 		findTheWay(point.coX, point.coY, number-1);
 		
-	}
-	
-//	public static void randomMap(CustomGameMapData mapData, int num){
-//		if(num == 0) return;
-//		Random random = new Random();
-//		
-//		int originX = random.nextInt(99);
-//		int originY = random.nextInt(99);
-//		randomDungeon(mapData, num, originX, originY);
-//	}
-	
-//	public static void randomDungeon(CustomGameMapData mapData, int num, int x, int y) {
-//		if(num == 0) return;
-//		mapData.map[0][x][y] = 1;
-//		num--;
-//		if(x-1 >= 0) randomDungeon(mapData, num, x-1, y);
-//		if(y-1 >= 0) randomDungeon(mapData, num, x, y-1);
-//		if(x+1 < SIZE) randomDungeon(mapData, num, x+1, y);
-//		if(y+1 < SIZE) randomDungeon(mapData, num, x, y+1);
-//	}	
-		
-		
-		
-		
-		
-		
-		
-//	public static void randomBase(CustomGameMapData mapData, int num, int x, int y) {
-//		if(num == 0) {
-//			return;
-//		}
-//		if(x - 2 < 0) {
-//			randomBase(mapData, num, 2, y);
-//			return;
-//		} 
-//		if(x + 2 >= SIZE) {
-//			randomBase(mapData, num, SIZE-3, y);
-//			return;
-//		}
-//		if(y - 1 < 0) {
-//			randomBase(mapData, num, x, 1);
-//			return;
-//		}
-//		if(y + 1 >= SIZE) {
-//			randomBase(mapData, num, x, SIZE-2);
-//			return;
-//		}
-//		Random random = new Random();
-//		mapData.map[0][x][y] = 1;
-//		mapData.map[0][x-1][y] = 1;
-//		mapData.map[0][x][y-1] = 1;
-//		mapData.map[0][x+1][y] = 1;
-//		mapData.map[0][x][y+1] = 1;
-//		num-=5;
-//		int d = random.nextInt(3);
-//		if(d == 0) {
-//			randomBase(mapData, num, x-1, y);
-//		}
-//		else if(d == 1) {
-//			randomBase(mapData, num, x, y-1);
-//		}
-//		else if(d == 2) {
-//			randomBase(mapData, num, x+1, y);
-//		}
-//		else if(d == 3) {
-//			randomBase(mapData, num, x, y+1);
-//		}
-//	}		
+	}		
 }
